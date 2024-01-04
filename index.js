10af2c72c74820d8adac403259b89b2c09d311ac
Adrian Ortiz <147290741+Agerian@users.noreply.github.com>
2024-01-03 20:56:32 -0700

    Debugging (no color)

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const generateSVG = require('./lib/svgGenerator');
 async function runApp() {
     try {
         const userInput = await getUserInput();
+        console.log('aoUser Input:', userInput);
         generateSVG(userInput);
     } catch(error) {
         console.error(`Error: ${error.message}`);
